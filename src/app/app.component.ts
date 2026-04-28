85fc741931415ac849f3513da1186e13ab2cee11
emna <mayna>
2026-04-28 10:41:54 +0100

    added searchbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, SearchBarComponent],
   template: `
     <app-header></app-header>
     <app-hero></app-hero>
+    <app-search-bar></app-search-bar>
     <router-outlet></router-outlet>
   `
 })
