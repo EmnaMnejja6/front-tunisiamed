a76483c68fd1534a04cd2468161f0ab7ad35f2ca
emna <mayna>
2026-04-28 18:46:38 +0100

    added cta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,19 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { StatsComponent } from './components/stats/stats.component';
+import { SpecialtiesComponent } from './components/specialties/specialties.component';
+import { CtaComponent } from './components/cta/cta.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, SearchBarComponent, StatsComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, SearchBarComponent, StatsComponent, SpecialtiesComponent, CtaComponent],
   template: `
     <app-header></app-header>
     <app-hero></app-hero>
     <app-search-bar></app-search-bar>
     <app-stats></app-stats>
+    <app-specialties></app-specialties>
+    <app-cta></app-cta>
     <router-outlet></router-outlet>
   `
 })
